b302c1ad5a0e1e477c490a19bd65b43fcd143bd0
guhlima-dev <gustavoliveira097@gmail.com>
2026-03-25 21:47:41 -0300

    Eu criei a classe HealthController, e adicionei as configurações do application.properties

---
+++ src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
@@ -0,0 +1,41 @@
+package com.deliverytech.delivery_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of(
+                "status", "UP",
+                "timestamp", LocalDateTime.now().toString(),
+                "service", "Delivery Api",
+                "javaVersion", System.getProperty("java.version")
+        );
+    }
+
+    @GetMapping("/info")
+    public AppInfo info() {
+        return new AppInfo(
+                "Delivery Tech Api",
+                "1.0.0",
+                "Gustavo de Lima",
+                "JDK 21",
+                "Spring Boot"
+        );
+    }
+
+    public record AppInfo(
+            String application,
+            String version,
+            String developer,
+            String javaVersion,
+            String framework
+    ){}
+
+}
